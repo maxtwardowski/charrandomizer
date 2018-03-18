3ecf21a0c65690a9efea25bef0a5565518fca3c2
maxtwardowski <twardowskimax@gmail.com>
2018-03-18 14:34:40 +0100

    Added a method which reads the .txt file content, builds a string out of it and prints to the console

--- src/charWriter.java
+++ src/charWriter.java
@@ -1,6 +1,8 @@
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.PrintWriter;
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class charWriter {
@@ -23,11 +25,24 @@ public class charWriter {
 			writer.println(RandomizeChar());
 		writer.close();
 	}
+	
+	void ReadAndPrint() throws FileNotFoundException {
+		Scanner scanner = new Scanner(new FileReader("randomchars.txt"));
+		StringBuilder builder = new StringBuilder();
+		while(scanner.hasNext()) {
+		    builder.append(scanner.next());
+		}
+		scanner.close();
+		
+		String readOut = builder.toString();
+		System.out.println(readOut);
+	}
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		charWriter wat = new charWriter(1000);
 		wat.WriteToFile();
+		wat.ReadAndPrint();
 		
 	}
 
